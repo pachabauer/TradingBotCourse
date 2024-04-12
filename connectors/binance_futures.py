b3be05644253cb3916dfee55a64aefce36a0a976
Esteban Bauer <eabestudio@gmail.com>
2024-04-12 14:55:47 -0300

    Hecho hasta la clase 12

---
+++ connectors/binance_futures.py
@@ -0,0 +1,81 @@
+import logging
+import requests
+#libreria para hacer más fácil y clara la lectura de los print()
+import pprint
+
+logger = logging.getLogger()
+
+# hacemos una clase que contendrá varios métodos relacionados
+class BinanceFuturesClient:
+
+    # el constructor es __init__
+    # testnet es un boolean que se usa de entrada (en este caso) para identificar si trabajamos
+    # sobre test o sobre real (true or false)
+    def __init__(self, testnet):
+        if testnet:
+            self.base_url = "https://testnet.binancefuture.com"
+        else:
+            self.base_url = "https://fapi.binance.com"
+
+        self.prices = dict()
+
+        logger.info("Binance Futures Client succesfully initialized")
+
+
+    # method se refiere a los de Http (GET, POST, etc) y el endpoint apunta a la url que usaremos (test o real)
+    def make_request(self, method, endpoint, data):
+        if method == "GET":
+            response = requests.get(self.base_url + endpoint, params=data)
+        else:
+            raise ValueError()
+
+        if response.status_code == 200:
+            return  response.json()
+        else:
+            # los %s son variables method y endpoint, response (los 2)
+            logger.error("Error while making %s request to %s: %s (error code %s)",
+                         method, endpoint, response.json(), response.status_code)
+            return None
+
+    def get_contracts(self):
+        # como parámetro data del método make_request no hace falta poner nada (ver docs Binance) , por eso va None
+        exchange_info = self.make_request("GET", "/fapi/v1/exchangeInfo", None)
+
+        contracts = dict()
+
+        if exchange_info is not None:
+            for contract_data in exchange_info['symbols']:
+                # estructura de diccionario (key,data), siendo el pair la key y la data es toda la lista
+                contracts[contract_data['pair']] = contract_data
+
+        return contracts
+
+    def get_historical_candles(self, symbol, interval):
+        data = dict()
+        data['symbol'] = symbol
+        data['interval'] = interval
+        data['limit'] = 1000
+
+        raw_candles = self.make_request("GET", "/fapi/v1/klines", data)
+
+        candles = []
+
+        if raw_candles is not None:
+            for c in raw_candles:
+                candles.append([c[0], float(c[1]), float(c[2]), float(c[3]), float(c[4]), float(c[5])])
+
+        return candles
+
+    def get_bid_ask(self, symbol):
+        data = dict()
+        data['symbol'] = symbol
+        ob_data = self.make_request("GET", "/fapi/v1/ticker/bookTicker", data)
+
+        if ob_data is not None:
+            if symbol not in self.prices:
+                self.prices[symbol] = {'bid': float(ob_data['bidPrice']), 'ask': float(ob_data['askPrice'])}
+            else:
+                self.prices[symbol]['bid'] = float(ob_data['bidPrice'])
+                self.prices[symbol]['ask'] = float(ob_data['askPrice'])
+
+        return self.prices[symbol]
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 import logging
-from bitmex_futures import get_instruments
+from connectors.binance_futures import BinanceFuturesClient
 
 logger = logging.getLogger()
 
@@ -31,41 +31,10 @@ logger.addHandler(file_handler)
 
 if __name__== '__main__':
 
-    bitmex_instruments = get_instruments()
+    binance = BinanceFuturesClient(True)
+    print(binance.get_historical_candles('BTCUSDT', "1h"))
 
     root = tk.Tk()
-
-    # de entrada defino el background color de la pantalla.
-    root.configure(bg="gray12")
-
-    i = 0
-    j = 0
-
-    # defino una tupla para el tipo de letra que van a llevar los widget
-    calibri_font = ("Calibri", 11, "normal")
-
-    # ahora vamos a crear los labels para que se vean en el widget (interface)
-    for instrument in bitmex_instruments:
-
-        # el método Label() necesita argumentos --> en donde va a ir del window (en este caso root, pero puede haber
-        # otras ventanas, el texto que va a mostrar ese label)
-        # también tiene la posibilidad de incorporar bordes y el relief es el tipo de borde
-        # bg es el background color y fg es el color y tipo de la font
-        label_widget = tk.Label(root, text= instrument, bg='gray12', fg='SteelBlue1', font= calibri_font, borderwidth=1, relief=tk.SOLID, width=13)
-
-        # también debo especificar (sino no se ven los labels) la forma de visualización: se pueden usar 2 métodos:
-        # pack() --> que hace que se vea cada label en una ventana separada abajo de otra. Será útil cuando hagamos
-        #            los 4 subframes de la interface, una con cada ventana.
-        # grid() --> que hace que se vea como filas. Este es el más indicado cuando queremos mostrar listas de datos.
-        # sticky es la forma de hacer que las columnas ocupen todo el espacio a la izquierda y derecha, a fin que
-        # queden uniformes.
-        label_widget.grid(row = i, column = j, sticky='ew')
-
-        if i == 4:
-            j +=1
-            i = 0
-        else:
-            i += 1
 
     # mainloop() hace que la ventana no se cierre automaticamente,
     # sino que se queda esperando hasta que el usuario realice una accion.
